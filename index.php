b2af5d4e2330ff6f44ec2b24fb76345fa5cfe613
Marine Boidé <boidemarine@hotmail.fr>
2021-09-04 16:14:05 +0200

    correction fichier supprimé

--- index.php
+++ index.php
@@ -54,14 +54,9 @@
            d'améliorer les conditions de vies ou d'hospitalisation.
         </p>
         <input type="button" value="En savoir plus" class="button_ensavoirplus">
-<<<<<<< HEAD
     </section>
     <aside>
         <img src="#" alt="Evénement" class="image_événement">
-=======
-    <aside>
-        <img src="" alt="Evénement" class="image_événement">
->>>>>>> main
         <input type="button" value="Prochain événement" class="button_prochainevenement">
     </aside>
     <footer>
@@ -86,12 +81,9 @@
             <img src="#" alt="Logo">
         </span>
     </footer>
-<<<<<<< HEAD
 
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-=======
->>>>>>> main
 </body>
 </html>
