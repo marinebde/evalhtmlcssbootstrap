958f3f86432cb47d7ad406f718b25cb7d5ec048c
Marine Boidé <boidemarine@hotmail.fr>
2021-09-04 15:22:00 +0200

    ajout du fichier index.php et style.css

---
+++ index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Un combat, un sourire. Association qui vient en aide aux enfants atteints de cancers.</title>
+    <script src="https://kit.fontawesome.com/7d8b466c07.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bitter:wght@400;500&family=EB+Garamond:wght@500;700&family=Roboto:wght@300;500&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <nav class="nav navbar navbar-expand-lg">
+            <button class="navbar-toggler " type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <img class="nav__logo" src="visuels/logo.png" alt="Logo">
+            <h1 class="nav__title">Un combat, <span class="span__title">un sourire</h1>
+            <div class="collapse navbar-collapse" id="navbarToggleExternalContent">
+            <ul class="navbar-nav">
+                <li class="nav__item nav-item">
+                    <a class="nav-link" href="#">Accueil</a>
+                </li>
+                <li class="nav__item nav-item">
+                    <a class="nav-link" href="#">Dons</a>
+                </li>
+                <li class="nav__item nav-item">
+                    <a class="nav-link" href="#">Evénements</a>
+                </li>
+                <li class="nav__item nav-item">
+                    <a class="nav-link" href="#">Contact</a>
+                </li>
+                <li class="nav__item nav-item">
+                    <a class="nav-link" href="#">A propos</a>
+                </li>
+            </ul>
+            </div>
+        </nav>
+    </header>
+    <main>
+        <img src="#" alt="Image principale" class="main_img">
+        <input type="button" value="Faire un Don pour l'association" class="main_button">
+    </main>
+    <section class="section_1">
+        <h2 class="section_titre">Notre Combat</h2>
+        <p class="section_paragraphe">L'association vient en aide aux familles et surtout aux enfants atteints de cancers
+           pédiatriques.
+           Chaque année, 1750 nouveaux cas de cancers sont recensés chez les enfants.
+           Un chiffre qui ne doit pas rester en suspend !
+           Pour cela nous organisons des collectes afin de financer les frais médicaux,
+           d'améliorer les conditions de vies ou d'hospitalisation.
+        </p>
+        <input type="button" value="En savoir plus" class="button_ensavoirplus">
+<<<<<<< HEAD
+    </section>
+    <aside>
+        <img src="#" alt="Evénement" class="image_événement">
+=======
+    <aside>
+        <img src="" alt="Evénement" class="image_événement">
+>>>>>>> main
+        <input type="button" value="Prochain événement" class="button_prochainevenement">
+    </aside>
+    <footer>
+        <ul>
+            <li class="footer_item">
+                <a href="">Contact</a>
+            </li>
+            <li class="footer_item">
+                <a href="">Mentions Légales</a>
+            </li>
+            <li class="footer_item">
+                <a href="">A propos</a>
+            </li>
+        </ul>
+        <div>
+            <a href="#" class="incon_facebook"><i class="fab fa-facebook"></i></a>
+            <a href="#" class="icon_twitter"><i class="fab fa-twitter"></i></a>
+            <a href="#" class="icon_instagram"><i class="fab fa-instagram"></i></a>
+
+        </div>
+        <span class="footer_logo">
+            <img src="#" alt="Logo">
+        </span>
+    </footer>
+<<<<<<< HEAD
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+=======
+>>>>>>> main
+</body>
+</html>
